401c31f549949162244281e03da1eaf2634b70d7
Isaac Matheus S. Novais <isaacmatheusnovais@gmail.com>
2026-09-16 22:02:23 -0300

    adiciona função de conexão com o banco de dados

---
+++ database.py
@@ -0,0 +1,18 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def conectar():
+    """
+    Cria e retorna uma conexão com o banco de dados PostgreSQL
+    utilizando as credenciais definidas no arquivo .env.
+    """
+    return psycopg2.connect(
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
